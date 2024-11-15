32bed354fad1551c0420e882f95f956e4b728008
Prajapati Pritamkumar Rameshbhai (CSS ICW ENG CPS SVHW V1 A) <PritamkumarRameshbhai.Prajapati@infineon.com>
2024-11-15 14:46:44 +0530

    lec2 files added

---
+++ lec2/index.js
@@ -0,0 +1,13 @@
+console.log("hello world");
+
+
+alert("this is pop up")
+
+
+let age =prompt("enter the age:");
+
+if(age>18){
+    console.log("you can vote");
+}else{
+    console.log("you can not vote");
+}
